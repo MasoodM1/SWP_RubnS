3d52245f21ab37c22915aab71e322e584747aa29
Masood <104983434+MasoodM1@users.noreply.github.com>
2024-09-30 20:03:54 +0200

    01_Lotto_UE

--- 01_Lotto_UE/main.py
+++ 01_Lotto_UE/main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def lotto_numbers():
+    numbers = list(range(1, 46))  # List of numbers from 1 to 45
+    number_lotto = []  # List to store drawn numbers
+
+    for i in range(6):
+        random_index = random.randint(0, 44 - i)
+        number_lotto.append(numbers[random_index])
+        numbers[random_index], numbers[44 - i] = numbers[44 - i], numbers[random_index] # Swap drawn number with last number in list
+
+    return number_lotto
+
+def lotto_statistic():
+    stats = {i: 0 for i in range(1, 46)}  # Initialize dictionary to count occurrences of each number
+
+    for _ in range(1000):
+        drawn_numbers = lotto_numbers()
+        for numbers in drawn_numbers:
+            stats[numbers] += 1
+
+    return stats
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Lotto numbers: ", lotto_numbers())
+    statistics = lotto_statistic()
+    for number, count in statistics.items():
+        print(f"Number {number}: {count} times")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
