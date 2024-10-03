24d8d3a3fc441f6f60a3a6ecefaab9c32c8924b5
Masood <104983434+MasoodM1@users.noreply.github.com>
2024-10-03 08:08:43 +0200

    01_Lotto_UE

--- 01_Lotto_UE/main.py
+++ 01_Lotto_UE/main.py
@@ -2,18 +2,18 @@ import random
 
 
 def lotto_numbers():
-    numbers = list(range(1, 46))  # List of numbers from 1 to 45
-    number_lotto = []  # List to store drawn numbers
+    numbers = list(range(1, 46))
+    number_lotto = []
 
     for i in range(6):
         random_index = random.randint(0, 44 - i)
         number_lotto.append(numbers[random_index])
-        numbers[random_index], numbers[44 - i] = numbers[44 - i], numbers[random_index] # Swap drawn number with last number in list
+        numbers[random_index], numbers[44 - i] = numbers[44 - i], numbers[random_index]
 
     return number_lotto
 
 def lotto_statistic():
-    stats = {i: 0 for i in range(1, 46)}  # Initialize dictionary to count occurrences of each number
+    stats = {i: 0 for i in range(1, 46)}
 
     for _ in range(1000):
         drawn_numbers = lotto_numbers()
